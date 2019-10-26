04910ab8b1aa676ac68e6c18c6ca0e7d616fd284
Ryunosuke Muramatsu <ryunosuke@altive.jp>
2019-10-26 12:46:12 +0900

    Initial Commit

---
+++ SwiftUI-Cheatsheet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Cheatsheet
+//
+//  Created by 村松龍之介 on 2019/10/26.
+//  Copyright © 2019 村松龍之介. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
